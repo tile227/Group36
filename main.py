b70ee45d101feb090a5deb3b72949ec84c3aa996
katVoll <s242086@dtu.dk>
2024-09-16 16:18:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import ifcopenshell
 
-from .rules import windowRule
-from .rules import doorRule
+from .rules import beamRule
+
 
 model = ifcopenshell.open("path/to/ifcfile.ifc")
 
-windowResult = windowRule.checkRule(model)
-doorResult = doorRule.checkRule(model)
+beamResult = beamRule.checkRule(model)
 
-print("Window result:", windowResult)
-print("Door result:", doorResult)
+
+print("Beam result:", beamResult)
+
